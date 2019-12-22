87a123e300842f41209c9f8fb9916debb271b802
MarcusJones <jones.0bj3@gmail.com>
2019-12-22 11:51:39 +0100

    update type examples

--- Section02/L1-using-types/app.js
+++ Section02/L1-using-types/app.js
@@ -1,1 +1,17 @@
-console.log('Test 1');
+console.log("Test 1");
+function add(n1, n2, showRes, phrase) {
+    console.log(typeof n1);
+    var res = n1 + n2;
+    if (showRes) {
+        console.log(phrase + res);
+    }
+    else {
+        return res;
+    }
+}
+var x1 = 5;
+var x2 = 2.8;
+var printResult = true;
+var resPhrase = 'Result is:';
+var res = add(x1, x2, printResult, resPhrase);
+console.log(res);
--- Section02/L1-using-types/app.ts
+++ Section02/L1-using-types/app.ts
@@ -1,1 +1,20 @@
-console.log('Test 1');
+console.log("Test 1");
+
+function add(n1: number, n2: number, showRes: boolean, phrase: string) {
+  console.log(typeof n1);
+  const res = n1 + n2;
+  if (showRes) {
+    console.log(phrase + res);
+  } else {
+    return res;
+  }
+}
+
+const x1 = 5;
+const x2 = 2.8;
+const printResult = true;
+const resPhrase = 'Result is:'
+
+const res = add(x1, x2, printResult, resPhrase);
+
+console.log(res);
