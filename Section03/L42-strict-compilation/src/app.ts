ef73885e7abd98ff832c9b05a2c38c152bf91563
MarcusJones <jones.0bj3@gmail.com>
2019-12-25 19:18:23 +0100

    add debug lecture

--- Section03/L42-strict-compilation/src/app.ts
+++ Section03/L42-strict-compilation/src/app.ts
@@ -1,2 +1,10 @@
-const userName = 'Maaa'
-console.log(userName)
+const button = document.querySelector('button')!;
+
+function clickHandler(message: string) {
+   console.log('Clicked! ' + message)
+}
+
+button.addEventListener('click', clickHandler.bind(null, 'Hello...'))
+// button.addEventListener('click', () => {
+//     console.log('CLICKED')
+// xx
