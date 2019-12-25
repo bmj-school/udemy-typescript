f7eb9b2d2bdb97fe587bc3032bd00cdfb8437181
MarcusJones <jones.0bj3@gmail.com>
2019-12-25 20:12:10 +0100

    update

--- Section04/L47-modern-js/src/app.ts
+++ Section04/L47-modern-js/src/app.ts
@@ -1,1 +1,12 @@
 // Code goes here!
+
+const add = (a: number, b: number) => {
+    return a+b
+}
+
+const printOutput = (output: string | number) => {
+    console.log(output)
+    
+}
+
+printOutput(add(5,2))
