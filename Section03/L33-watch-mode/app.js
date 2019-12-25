f8640f97629d2a146d73de96f62eafe796067544
MarcusJones <jones.0bj3@gmail.com>
2019-12-25 16:11:21 +0100

    add section 3 lecture

--- Section03/L33-watch-mode/app.js
+++ Section03/L33-watch-mode/app.js
@@ -1,10 +1,3 @@
-var userInput;
-// let userInput: any
-var userName;
-userInput = 34;
-userInput = 'Maasdf';
-/* userName = userInput // Error here! */
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An Error!', 500);
+"use strict";
+var userName = 'Maaa';
+console.log(userName);
--- Section03/L33-watch-mode/app.ts
+++ Section03/L33-watch-mode/app.ts
@@ -1,17 +1,2 @@
-let userInput: unknown
-// let userInput: any
-
-let userName: string
-
-
-userInput = 34
-userInput = 'Maasdf'
-
-/* userName = userInput // Error here! */
-
-
-function generateError(message:string, code:number): never{
-  throw {message: message, errorCode: code};
-  }
-
-generateError('An Error!', 500)
+const userName = 'Maaa'
+console.log(userName)
