ab3062db6677e30d5e3ec0363df1cc20f53724a8
MarcusJones <jones.0bj3@gmail.com>
2019-12-26 08:55:58 +0100

    add finished section04

--- Section04/L47-modern-js/src/app.ts
+++ Section04/L47-modern-js/src/app.ts
@@ -1,6 +1,6 @@
 // Code goes here!
 
-const add = (a: number, b: number) => {
+const add = (a: number, b: number=1) => {
     return a+b
 }
 
@@ -9,3 +9,34 @@ const printOutput = (output: string | number) => {
 }
 
 printOutput(add(5,2))
+printOutput(add(5))
+
+const hobbies = ['sports', 'cooking']
+
+const activeHobbies = ['hiking']
+
+activeHobbies.push(...hobbies)
+
+const person = {
+    name: 'asdf',
+    age: 3
+}
+
+const copiedPerson = { ...person }
+
+const addAll = (...numbers: number[]) => {
+    return numbers.reduce((curRes, curVal) => {
+        return curRes + curVal
+    }, 0)
+}
+
+const addedNumbers = addAll(5,10,11,3.7)
+console.log(addedNumbers)
+
+const [h1, h2, ...remainingH] = hobbies
+
+const person2 = {
+    nameA: 'asdf',
+    ageA: 3
+}
+const { nameA, ageA } = person2
